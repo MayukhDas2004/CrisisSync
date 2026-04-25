7f2efa87a4fda2204b51445405cb09c3dab2a685
Megha Pattanayak <meghinblack10@gmail.com>
2026-04-25 14:44:01 +0530

    Update App.js

--- frontend/dashboard/src/App.js
+++ frontend/dashboard/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Signup from './pages/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
